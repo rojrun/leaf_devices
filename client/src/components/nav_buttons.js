083586263b8bed62efcdcdd1da203ee6703934cf
rojrun <runroj@gmail.com>
2019-01-09 10:04:45 -0800

    working on click handlers

--- client/src/components/nav_buttons.js
+++ client/src/components/nav_buttons.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/nav_buttons.css';
 
-export default () => {
+class NavButtons extends Component {
 
-    return (
-        <div className="buttons">
-            <button className="waves-effect waves-light btn-small">home</button>
-            <button className="waves-effect waves-light btn-small">cart</button>
-            <button className="waves-effect waves-light btn-small">contact</button>
-            <button className="waves-effect waves-light btn-small">create</button>
-            <button className="waves-effect waves-light btn-small">login</button>
-        </div>
-    );
+    handleHomeClick(event) {
+        console.log('home click:', event);
+    }
+
+    handleCartClick(event) {
+
+    }
+
+    handleContactClick(event) {
+
+    }
+
+    handleCreateClick(event) {
+
+
+    }
+
+    handleLoginClick(event) {
+
+    }
+
+    render() {
+        return (
+            <div className="buttons">
+                <button className="waves-effect waves-light btn-small" onChange={this.handleHomeClick.bind(this)}>home</button>
+                <button className="waves-effect waves-light btn-small" onChange={this.handleCartClick.bind(this)}>cart</button>
+                <button className="waves-effect waves-light btn-small" onChange={this.handleContactClick.bind(this)}>contact</button>
+                <button className="waves-effect waves-light btn-small" onChange={this.handleCreateClick.bind(this)}>create</button>
+                <button className="waves-effect waves-light btn-small" onChange={this.handleLoginClick.bind(this)}>login</button>
+            </div>
+        );
+    }
 }
+
+export default NavButtons;
