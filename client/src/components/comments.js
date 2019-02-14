2f32f08ba90ff72dda4070823c39c9f909323fb1
rojrun <runroj@gmail.com>
2019-02-14 15:27:33 -0800

    fixed form

--- client/src/components/comments.js
+++ client/src/components/comments.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import '../assets/css/comments.css';
     
-<<<<<<< HEAD
-/* Comments component displays message from props */
-=======
 /* Comment component using logo, with message passed through props */
->>>>>>> 05a7c81ed781baf3312162ed5e92b3c076ddb91e
 const Comments = (props) => {
     return (
         <div className="status">
