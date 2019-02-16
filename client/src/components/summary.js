0b906bcb5c96f6625f26b9c664702c762ff783ce
rojrun <runroj@gmail.com>
2019-02-16 15:52:59 -0800

    shipping selection work in progress

--- client/src/components/summary.js
+++ client/src/components/summary.js
@@ -6,8 +6,27 @@ import {addToSummary, getSummary} from '../actions';
 class Summary extends Component {
 
     componentDidMount() {
+        this.instances = M.FormSelect.init(this.formSelect);
         this.props.addToSummary();
         this.props.getSummary();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.summary !== this.props.summary) {
+            this.instances = M.FormSelect.init(this.formSelect);
+        }      
+    }
+
+    shippingMethods = () =>  {
+        return (
+            <div className="input-field">
+                <select ref="dropdown" defaultValue="1" className="browser-default">
+                    <option value="1">Standard Shipping: </option>
+                    <option value="2">Expedited Shipping: </option>
+                </select>
+                <label>Select Shipping Method</label>
+            </div>
+        );
     }
 
     handleCheckout = () => {
@@ -22,7 +41,13 @@ class Summary extends Component {
                 <p><b>Total Quantity: </b>{total_quantity}</p>
                 <p><b>Subtotal: </b>{subtotal/100}</p>
                 <p><b>Tax: </b>{tax/100}</p>
-                <p><b>Shipping: </b>{shipping/100}</p>
+
+                {/* <p><b>Shipping: </b>{shipping/100}</p> */}
+                <div className="row">
+                    <b>{this.shippingMethods}</b>
+                    {shipping}
+                </div>
+
                 <p><b>Total: ${total/100}</b></p>
                 <button onClick={this.handleCheckout} className="checkoutButton waves-effect waves-light btn">checkout</button>
             </div>
